b562ec0f3d9ba59f24ee8626331fda8ccabc362a
csmht <3299524971@qq.com>
2025-10-26 23:26:13 +0800

    1

--- src/main/java/csmht/dao/Constant.java
+++
@@ -1,7 +0,0 @@
-package csmht.dao;
-
-
-public class Constant {
-    public static final String New = "ORDER BY create_time DESC";
-    public static final String Hot = "ORDER BY likes DESC";
-}
--- src/test/java/text.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public class text {
-
-   
-
-
-}
